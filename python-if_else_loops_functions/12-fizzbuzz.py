c72bfff091e8145cf531ab6091a07ddbcd7eb194
kingdavidch <kingdavidscloud@gmail.com>
2023-07-10 14:56:39 +0000

    commit8

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
+
+
 def fizzbuzz():
-for i in range(1, 101):
-if i % 5 == 0 and i % 3 == 0:
-            print("{}".format("FizzBuzz"), end=" ")
-elif i % 3 == 0:
-            print("{}".format("Fizz"), end=" ")
-elif i % 5 == 0:
-            print("{}".format("Buzz"), end=" ")
-else:
-            print("{}".format(i), end=" ")
+    for i in range(1, 101):
+        if (i % 3 == 0 and i % 5 == 0):
+            print("FizzBuzz", end=" ")
+        elif (i % 3 == 0):
+            print("Fizz", end=" ")
+        elif (i % 5 == 0):
+            print("Buzz", end=" ")
+        else:
+            print(f"{i}", end=" ")
