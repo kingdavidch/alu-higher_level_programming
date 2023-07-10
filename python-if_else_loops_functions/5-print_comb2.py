7aef65393dc2205a2acbde16b6964f720b05bad1
kingdavidch <kingdavidscloud@gmail.com>
2023-07-10 13:47:40 +0000

    commit2

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-        if i == 99:
-                    print("{}".format(i))
-                        else:
-                                    print("{:02}".format(i), end=", ")
+
+for i in range(100):
+    if (i != 99):
+        print("{num:02}".format(num=i), end=", ")
+    else:
+        print("{num:02}".format(num=i), end="\n")
